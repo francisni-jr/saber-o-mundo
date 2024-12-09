f4d73895572c7ce0a8d8b47cc28a7b99030515b1
Francisní <francisnijr1.pro@gmail.com>
2024-12-09 20:40:20 -0300

    Adding about page, more info

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -2,7 +2,7 @@
     
     <p class="mb-20 text-start border-l p-5">O mundo é uma "bolha" onde nós devemos ser a luz que reflete nela</p>
 
-    <h1 class="text-6xl font-title mb-5">Sobre Nós</h1>
+    <h1 class="text-6xl font-title mb-10">Sobre Nós</h1>
     <p class="mb-10">Somos um meio de transmissão de informação online, utilizando das redes sociais para influenciar as pessoas de maneira positiva</p>
 
     <p class="mb-10">Aprenda sobre diferentes aspectos socioculturais do mundo moderno</p>
@@ -16,6 +16,9 @@
         <li class="font-bold">Filosofia</li>
         <li class="">E muito mais...</li>
     </ul>
+
+    <h1 class="text-6xl font-title mb-10">Mais afundo</h1>
+    <p class="mb-10" >"Saber o Mundo" é um projeto que se dedica a explorar e compartilhar conhecimento sobre cultura e sociedade de maneira acessível e envolvente. Nosso objetivo é criar conteúdos que não apenas informem, mas também inspirem o público a refletir sobre o mundo ao seu redor.  <br><br> Nós, do "Saber o Mundo", acreditamos que o conhecimento é uma ferramenta poderosa para promover a compreensão mútua e o respeito entre diferentes culturas e perspectivas. Por isso, nos esforçamos para produzir vídeos e materiais que abordem temas relevantes e atuais, sempre com um olhar crítico e ético. <br><br> A ética é um pilar fundamental em nosso trabalho. Comprometemo-nos a tratar todos os temas com responsabilidade, evitando a disseminação de informações enganosas ou preconceituosas. Valorizamos a diversidade de opiniões e incentivamos o diálogo construtivo, pois acreditamos que é através da troca de ideias que podemos crescer como indivíduos e como sociedade. <br> Além disso, "Saber o Mundo" busca ser uma fonte de inspiração para aqueles que desejam expandir seus horizontes e compreender melhor as complexidades do mundo moderno. Queremos que nosso público se sinta encorajado a questionar, aprender e compartilhar suas próprias experiências e conhecimentos. <br><br> Em resumo, "Saber o Mundo" é mais do que um simples canal de conteúdo; é uma comunidade de aprendizado contínuo, onde cada elemento é uma oportunidade de crescimento pessoal e coletivo. Se você está em busca de um espaço para explorar novas ideias e se conectar com pessoas que compartilham o mesmo desejo de saber mais sobre o mundo, você está no lugar certo.</p>
 
     <p class="my-10">Conheça os nossos recursos e saiba mais sobre o que postamos nas redes sociais:</p> 
     <a href="/descobrir" class="text-p text-xl bg-gradient-to-r from-s to-t p-5 rounded-xl">Descobrir</a>
