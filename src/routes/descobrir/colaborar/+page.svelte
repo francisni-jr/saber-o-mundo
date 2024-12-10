8c3a3fac3552e2b377602559313eb51db5e94ad3
Francisní <francisnijr1.pro@gmail.com>
2024-12-09 21:47:58 -0300

    Adding instructions to colaborate

--- src/routes/descobrir/colaborar/+page.svelte
+++ src/routes/descobrir/colaborar/+page.svelte
@@ -9,16 +9,17 @@
     </div>
     <ul class="flex flex-col gap-5">
         <li class="">1. Entre no <a target="_blank" href="https://whatsapp.com/channel/0029Vay4GvJ6BIEpMiwdD43i" class="text-t">canal do whatsapp</a></li>
-        <li class="">2. Siga as intruções para entrar na comunidade
+        <li class="">2. Na descrição do canal clique no link para entrar na comunidade <br> <div class="flex w-full h-auto justify-center items-center my-10">
+            <img src="https://res.cloudinary.com/dncs4zash/image/upload/v1733790818/Design_sem_nome_3_gjqnja.png" class=" w-[50%] bg-gradient-to-r from-s to-t rounded-xl drop-shadow-[0_0_10px_red] aspect-auto hover:scale-[1.1] hover:cursor-cell"/>
+        </div>
         </li>
-        <li class="">3. Aguarde os avisos e solicite a entrada para o grupo "colaborações", então aguarde a aprovação</li>
-        <li class="">4. Após ser aprovado no grupo envie sua idéia de vídeo, o "@"" da sua rede social preferida e aguarde a nossa aprovação</li>
-        <li class="">5. Se sua idéia for aprovada, você poderá participar do conteúdo e marcaremos sua rede na descrição dos vídeos do youtube ou em outras mídias, como preferir</li>
-        <li class="">6. Agora, entrando no grupo vídeos vamos discutir a elaboração da sua idéia e suas preferências de reconhecimento</li>
-        <li class="font-bold mt-5">Pronto! <br><br> Agora você já é um colaborador aficial do Saber o Mundo</li>
+        <li class="">3. Peça pra entrar no grupo "colaborações" e aguarde a aprovação</li>
+        <li class="">4. Após ser aprovado no grupo envie sua idéia de colaboração, o "@"" da sua rede social preferida e em seguida peça para entrar no grupo "vídeos"</li>
+        <li class="">5. Caso gostarmos da sua idéia você poderá nos ajudar na elaboração da mesma</li>
+        <li class="">6. Após colaborar, o removeremos do grupo vídeos e faremos o mesmo processo novamente.</li>
     </ul>
     <h2 class="text-6xl font-title mt-20">Vantagens</h2>
-    <ul class="flex flex-col gap-5 mt-10">
+    <ul class="flex flex-col gap-5 mt-10 font-bold">
         <li class="">
             Menção em redes sociais
         </li>
